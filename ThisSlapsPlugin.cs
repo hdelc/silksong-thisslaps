a3c70eead985b068f604b356f335871b4a611afc
hdelc <hdelc02@gmail.com>
2025-09-29 18:43:48 -0500

    Implement slap key config

--- ThisSlapsPlugin.cs
+++ ThisSlapsPlugin.cs
@@ -1,55 +1,48 @@
-using System;
 using System.Collections;
-using System.IO;
-using System.Linq;
 using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
-using HutongGames.PlayMaker;
-using HutongGames.PlayMaker.Actions;
 using InControl;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 
 namespace ThisSlaps;
 
-// TODO - adjust the plugin guid as needed
 [BepInAutoPlugin(id: "dev.hdelc.ThisSlaps")]
 public partial class ThisSlapsPlugin : BaseUnityPlugin
 {
-    public static SlapActionSet inputActions;
+    public static SlapActionSet? inputActions;
     public static ManualLogSource Log;
-    
+    public static ThisSlapsPlugin instance = null!;
+
     private AsyncOperationHandle<AudioClip> _slapAudioHandle;
     private AudioClip _slapAudio;
     private SlapController _slapController;
     private Harmony _harmony;
+    private ConfigEntry<Key> _slapKeybindConfig;
+
     private void Awake()
     {
-        // Put your initialization logic here
+        instance = this;
         Log = Logger;
         Log.LogInfo($"Plugin {Name} ({Id}) has loaded!");
         _harmony = new Harmony(Id);
         _harmony.PatchAll(typeof(HeroPatches));
-        
+
         SetupInputs();
         StartCoroutine(LoadSlapAudio());
-
-        // var myLoadedAssetBundle 
-        //     = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "aa/StandaloneLinux64/sfxstatic_assets_areabellareacoralareagreymoorareashellwoodareasong"));
-        // Logger.LogInfo(Application.streamingAssetsPath);
-        // if (myLoadedAssetBundle == null) {
-        //     Debug.Log("Failed to load AssetBundle!");
-        //     return;
-        // }
-        // _slapAudio = myLoadedAssetBundle.LoadAsset<AudioClip>("hornet_slap_B_2");
-
-        // PlayMakerFSM fsm = new PlayMakerFSM()
     }
 
     private void SetupInputs()
     {
+        _slapKeybindConfig = Config.Bind(
+            "Inputs",
+            "Slap Binding",
+            Key.Semicolon,
+            "Slap action binding"
+        );
+        _slapKeybindConfig.SettingChanged += (_, _) => UpdateSlapBind();
         if (!InputManager.IsSetup)
         {
             InputManager.OnSetupCompleted += SetupInputsAction;
@@ -63,7 +56,18 @@ public partial class ThisSlapsPlugin : BaseUnityPlugin
     private void SetupInputsAction()
     {
         inputActions = new SlapActionSet();
-        inputActions.slapAction.AddBinding(new KeyBindingSource(Key.Semicolon));
+        UpdateSlapBind();
+    }
+
+    private void UpdateSlapBind()
+    {
+        if (inputActions is null)
+        {
+            return;
+        }
+
+        inputActions.slapAction.ClearBindings();
+        inputActions.slapAction.AddBinding(new KeyBindingSource(_slapKeybindConfig.Value));
     }
 
     private IEnumerator LoadSlapAudio()
@@ -91,35 +95,4 @@ public partial class ThisSlapsPlugin : BaseUnityPlugin
         _harmony.UnpatchSelf();
         AssetManager.UnloadManualBundles();
     }
-
-    private void Update()
-    {
-        // if (Input.GetKeyDown(KeyCode.F9))
-        // {
-        //     Logger.LogInfo("Patching silkSpecialFSM...");
-        //     PatchSilkFSM();
-        // }
-
-        // if (Input.GetKeyDown(KeyCode.Semicolon))
-        // {
-        // var hc = GameManager.instance.hero_ctrl;
-        // var clip = hc.animCtrl.GetClip("Idle Slap");
-        // AudioSource playerSource = hc.audioSource;
-        // hc.StopAnimationControl();
-        // hc.RelinquishControl();
-        // hc.animCtrl.animator.AnimationCompleted += ReturnAnimationControlHandler;
-        // hc.animCtrl.animator.Play(clip);
-        // playerSource.PlayOneShot(_slapAudio);
-        // }
-    }
-
-    private void ReturnAnimationControlHandler(tk2dSpriteAnimator animator, tk2dSpriteAnimationClip clip)
-    {
-        var hc = GameManager.instance.hero_ctrl;
-        hc.StartAnimationControl();
-        hc.RegainControl();
-        hc.animCtrl.animator.AnimationCompleted -= ReturnAnimationControlHandler;
-    }
-
-    
 }
